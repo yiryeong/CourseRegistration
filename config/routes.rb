6f1e5d287e1e1decb7a58763db59dd5bca45e3b3
yiryeong0720 <yiryeong0720@gmail.com>
2022-02-16 15:25:28 +0900

    1. add user controllers 2. change after_sign_up_path_for method    ㄴ> if sign up success to root_url
    Signed-off-by: yiryeong0720 <yiryeong0720@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   #
   root "home#index"
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
